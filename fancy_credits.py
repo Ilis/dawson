c279e8bfe7ac505798a413cb15c34ffc16f8a838
Ilis <ilis@inbox.ru>
2020-07-15 21:09:41 +0500

    fancy credits

---
+++ fancy_credits.py
@@ -0,0 +1,8 @@
+# Credits
+print("\t\t\tFancy credits")
+print("\t\t\t\\ \\ \\ \\ \\ \\ \\")
+print("\t\t\tMichael Dawson")
+print("\t\t\t\\ \\ \\ \\ \\ \\ \\")
+print("\t\t\tGame developer")
+print("\t\t\t\\ \\ \\ \\ \\ \\ \\")
+print("\nSpecial thanks to my hairdresser!")
