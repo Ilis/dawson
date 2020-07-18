dcca00a02f07c393dd8af230258ba7229b6ceed5
Ilis <ilis@inbox.ru>
2020-07-18 10:54:07 +0500

    word problems

---
+++ word_problems.py
@@ -0,0 +1,16 @@
+# Math
+
+print("Problem: 800 - 40 + 20 = ?")
+print("Answer:", 800 - 40 + 20)
+
+print("Problem: 6 * 3 = ?")
+print("Answer:", 6 * 3)
+
+print("Problem: 19 div 4 = ?")
+print("Answer:", 19 / 4)
+
+print("Problem: quotient of 107 and 4 = ?")
+print("Answer:", 107 // 4)
+
+print("Problem: remainder of 107 and 4 = ?")
+print("Answer:", 107 % 4)
