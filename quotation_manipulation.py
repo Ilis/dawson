a51492e33bdf8f6dbe18a22ea42bde36923b82a9
Ilis <ilis@inbox.ru>
2020-07-25 15:19:43 +0500

    quotaton man

---
+++ quotation_manipulation.py
@@ -0,0 +1,16 @@
+# Quotation manipulations
+
+quote = "I think there is a world market for maybe five computers."
+
+print("\nOrigin:")
+print(quote)
+print("\nUpper:")
+print(quote.upper())
+print("\nLower:")
+print(quote.lower())
+print("\nTitle:")
+print(quote.title())
+print("\nReplace:")
+print(quote.replace("five", "some mils"))
+print("\nOrigin:")
+print(quote)
